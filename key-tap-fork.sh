e3459210e060b9500a5326c0ff2e2b3589bf5d3a
Christopher Brown <io@henrian.com>
2014-03-07 23:07:41 -0600

    Dont need to re-sudo

--- key-tap-fork.sh
+++ key-tap-fork.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as sudo"
   exit 1
 fi
 
-sudo ./key-tap </dev/null >/dev/null 2>/dev/null &
+./key-tap </dev/null >/dev/null 2>/dev/null &
